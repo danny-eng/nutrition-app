ceb1c49addfd61817904fb3da1c00a1fe17e0c6d
Danny Eng <dannyeng94@gmail.com>
2017-12-13 11:14:50 -0500

    component files

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      search: "",
+      searchSave: "",
+      data: {}
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleForm = this.handleForm.bind(this)
+    this.getSearch = this.getSearch.bind(this)
+  }
+
+  handleChange(e){
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+  handleForm(e){
+    let searchSave = e.target.search.value
+    e.preventDefault()
+    this.setState({
+      searchSave: searchSave
+    })
+    this.getSearch(searchSave)
+  }
+
+  getSearch(search){
+    fetch(`https://api.nal.usda.gov/ndb/search/?format=json&q=${search}&sort=n&max=25&offset=0&api_key=QIH0VoWhfdeREixvllH9ohRQLHfp9TPKk5F78Owm`)
+    .then(res => res.json())
+    .then(res => {
+      this.setState({
+        data: res
+      })
+    })
+  }
+
   render() {
+    console.log(this.state.data)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleForm}>
+          <input
+            name="search"
+            type="textbox"
+            placeholder="Enter your search here"
+            value={this.state.search}
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Search" />
+        </form>
       </div>
-    );
+    )
   }
+
 }
 
-export default App;
+export default App
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   delete "/logout"      => "sessions#destroy"
   get "/profile"        => "users#profile"
   resources :users
-
+  resources :favorites
 end
